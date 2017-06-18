f8d223257fd0d4cd1b89cef60056c420c535ad43
HwarangKim <checheche12@naver.com>
2017-06-19 03:06:23 +0900

    Complete 16 Puzzle

--- hello.py
+++ hello.py
@@ -7,7 +7,34 @@ from pygame.locals import *
 class GameBoard():
     def __init__(self) :
         numberList = [i for i in range(16)]
-        random.shuffle(numberList)
+        while True :
+            random.shuffle(numberList)
+            reverse_number_count = 0
+            reverse_check = 0
+            zero=0
+            while reverse_check < 15 :
+                if(numberList[reverse_check]==0) :
+                    zero = reverse_check
+                    reverse_check+=1
+                    continue
+                reverse_check2 = reverse_check + 1
+                while reverse_check2 < 16 :
+                    if(numberList[reverse_check2]==0) :
+                        reverse_check2+=1
+                        continue
+                    elif(numberList[reverse_check] > numberList[reverse_check2]) :
+                        reverse_check2+=1
+                        reverse_number_count+=1
+                        continue
+                    reverse_check2+=1
+                reverse_check+=1
+
+            if(numberList[15]==0):
+                zero = 15
+            print(reverse_number_count+((zero//4)+1))
+            if (reverse_number_count+((zero//4)+1))%2 == 0 :
+                break;
+
         count = 0
         while count<4 :
             self.gameBoard[count]=[numberList[4*count],numberList[4*count+1],numberList[4*count+2],numberList[4*count+3]]
@@ -22,6 +49,29 @@ class GameBoard():
             self.gameBoard[clickY][clickX+1], self.gameBoard[clickY][clickX] = self.gameBoard[clickY][clickX], self.gameBoard[clickY][clickX+1]
         if clickY<=2 and self.gameBoard[clickY+1][clickX] == 0 :
             self.gameBoard[clickY+1][clickX], self.gameBoard[clickY][clickX] = self.gameBoard[clickY][clickX], self.gameBoard[clickY+1][clickX]
+
+        reverse_number_count = 0
+        reverse_check = 0
+        zero = 0
+        gBr = self.gameBoard[0]+self.gameBoard[1]+self.gameBoard[2]+self.gameBoard[3]
+        while reverse_check < 15 :
+            if(gBr[reverse_check]==0) :
+                zero = reverse_check
+                reverse_check+=1
+                continue
+            reverse_check2 = reverse_check + 1
+            while reverse_check2 < 16 :
+                if(gBr[reverse_check2]==0) :
+                    reverse_check2+=1
+                    continue
+                elif(gBr[reverse_check] > gBr[reverse_check2]) :
+                    reverse_check2+=1
+                    reverse_number_count+=1
+                    continue
+                reverse_check2+=1
+            reverse_check+=1
+        if(gBr[15]==0):
+            zero = 15
 
     def complete(self):
         if self.gameBoard == self.completeGameBoard :
@@ -50,51 +100,56 @@ BLACK = (0,0,0)
 RED = (255,0,0)
 GREEN = (0,255,0)
 LEFT = 1
+RIGHT = 3
 
 pygame.init()
 screen = pygame.display.set_mode((400, 400), DOUBLEBUF)
 loadImage = 0
 img=[]
+cong = pygame.image.load("Image/congraturation.png")
 
 while loadImage<16 :
     tempImage = pygame.image.load("Image/"+str(loadImage)+".png")
     img.append(tempImage)
     loadImage+=1
 
-gameBoard = GameBoard()
-while True :
-    if gameBoard.wrongKey == 1 :
-        print("제시된 네가지의 방향키중 하나만 눌러주십시오")
-        gameBoard.wrongKey=0
-    if gameBoard.wrongKey == 2 :
-        print("이동불가능한 장소로의 이동입니다.")
-        gameBoard.wrongKey=0
-    if gameBoard.complete() :
-        break;
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-        elif event.type == MOUSEBUTTONDOWN and event.button == LEFT :
-            li = event.pos
-            x = li[0]//100
-            y = li[1]//100
-            gameBoard.move(x,y)
 
-    screen.fill(WHITE)
-    gameBoard.printGameBoard()
-    pygame.display.flip()
-    clock.tick(TARGET_FPS)
-    '''key = input("a 왼쪽 s 아래쪽 d 오른쪽 w 위쪽 으로 0 이 이동합니다.")
-    if key == "a" :
-        gameBoard.left()
-    elif key == "s" :
-        gameBoard.down()
-    elif key == "d" :
-        gameBoard.right()
-    elif key == "w" :
-        gameBoard.up()
-    else :
-        gameBoard.wrongKey=1'''
+gameStatus = True
+gamePlay = True
+while gameStatus :
+    gameBoard = GameBoard()
+    while gamePlay :
+        if gameBoard.complete() :
+            gamePlay = False
+            break;
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == MOUSEBUTTONDOWN and event.button == LEFT :
+                li = event.pos
+                x = li[0]//100
+                y = li[1]//100
+                gameBoard.move(x,y)
 
-print("congraturation")
+        screen.fill(WHITE)
+        gameBoard.printGameBoard()
+        pygame.display.flip()
+        clock.tick(TARGET_FPS)
+
+    aaa = True;
+    while aaa :
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == MOUSEBUTTONDOWN and event.button == LEFT :
+                gamePlay = True
+                aaa = False
+                break;
+            elif event.type == MOUSEBUTTONDOWN and event.button == RIGHT :
+                pygame.quit()
+                sys.exit()
+        screen.blit(cong,(100,100))
+        pygame.display.flip()
+        clock.tick(5)
--- test.py
+++ test.py
@@ -2,17 +2,24 @@ import pygame
 from pygame.locals import *
 import sys
 
-pygame.init()
-screen = pygame.display.set_mode((1024,768),DOUBLEBUF)
-pygame.display.set_caption('Pracitce')
+numberList = [3,5,11,2,10,4,15,1,8,0,7,14,13,6,12,9]
+reverse_number_count = 0
+reverse_check = 0
+while reverse_check < 15 :
+    if(numberList[reverse_check]==0) :
+        reverse_check+=1
+        continue
+    reverse_check2 = reverse_check + 1
+    while reverse_check2 < 16 :
+        if(numberList[reverse_check2]==0) :
+            reverse_check2+=1
+            continue
+        if(numberList[reverse_check] > numberList[reverse_check2]) :
+            reverse_check2+=1
+            reverse_number_count+=1
+            continue
+        reverse_check2+=1
+    reverse_check+=1
 
-while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
 
-    screen.fill((255,255,255))
-    imageFile = pygame.image.load('Image/1.png')
-    screen.blit(imageFile, (50,100))
-    pygame.display.flip()
+print(reverse_number_count)
